8b4304b4ede334bb0f48de8831a657eb2b8ea444
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-05-07 05:49:07 -0700

    Add prior probability to classification error; win -> unix

--- R/mis_rate.R
+++ R/mis_rate.R
@@ -9,20 +9,23 @@
 ##' @param sigma1 covariance matrix of class 1
 ##' @return classification error
 ##' @author Ruiyang Wu
+##' @export
 
-
-mis_rate <- function(a, mu0, mu1, sigma0, sigma1) {
+mis_rate <- function(a, mu0, mu1, sigma0, sigma1, p0, p1) {
     m0 <- t(a) %*% mu0; m1 <- t(a) %*% mu1
     s0 <- sqrt(t(a) %*% sigma0 %*% a); s1 <- sqrt(t(a) %*% sigma1 %*% a)
-    delta <- sqrt((m0 - m1)^2 + (s0^2 - s1^2) * log(s0^2 / s1^2))
-    if (s0 == s1) {
+    dm <- m1 - m0
+    ds2 <- s0 ^ 2 - s1 ^ 2
+    delta <- sqrt((dm)^2 + ds2 * log(s0^2 / s1^2))
+    temp <- p1 * pnorm((s1 * dm - s0 * delta) / ds2) -
+        p1 * pnorm((s1 * dm + s0 * delta) / ds2) +
+        p0 * pnorm((s0 * dm + s1 * delta) / ds2) -
+        p0 * pnorm((s0 * dm - s1 * delta) / ds2)
+    if (identical(s0, s1)) {
         s <- s0
-        return(pnorm(-abs(m0 - m1) / (2 * s)))
-    } else
-        return((1 / 2) *
-               (pnorm((s1 * (m1 - m0) - s0 * delta) / (s0^2 - s1^2)) -
-                pnorm((s1 * (m1 - m0) + s0 * delta) / (s0^2 - s1^2)) +
-                pnorm((s0 * (m1 - m0) + s1 * delta) / (s0^2 - s1^2)) -
-                pnorm((s0 * (m1 - m0) - s1 * delta) / (s0^2 - s1^2)) +
-                1))
+        return(pnorm(-abs(dm) / (2 * s)))
+    } else if (s0 > s1)
+        return(p1 + temp)
+    else
+        return(p0 + temp)
 }
