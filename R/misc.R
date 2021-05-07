a96708c944a4238f49d543108d1d82cb4c8168be
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-05-07 06:15:18 -0700

    Allow to pass prior prob and optimization method to our method

--- R/misc.R
+++ R/misc.R
@@ -15,8 +15,9 @@ maxquadratio <- function(sigma0, sigma1) {
 
 is_invertible <- function(m) class(try(solve(m), silent = T))[1] == "matrix"
 
-drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma,
-                      lambda = 0, method = "Penalization") {
+drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
+                      lambda = 0, method = "Penalization",
+                      optim = "BFGS") {
     ## initialization
     if (method != "Penalization" & method != "Thresholding")
         stop("Wrong method")
@@ -27,8 +28,18 @@ drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma,
                               lambda * sum(abs(a))
     ## optimization wrt lda direction as initial
     if (lambda == 0 | method == "Thresholding") {
-        op <- optim(par = a0, fn = mis_rate, method = "BFGS",
-                    mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1)
+        if (optim == "BFGS")
+            op <- optim(par = par0, fn = mis_rate, method = "BFGS",
+                        mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
+                        p0 = p0, p1 = p1, control = list(maxit = 1000))
+        else if (optim == "codesc")
+            op <- codesc(par = par0, fun = mis_rate, max_iter = 1000,
+                         mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
+                         p0 = p0, p1 = p1, step_size = 0.1)
+        else
+            op <- frank_wolfe(par = par0, fun = mis_rate, max_iter = 500,
+                              mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
+                              p0 = p0, p1 = p1)
         d <- op$par
         conv <- op$convergence
     } else{
@@ -72,24 +83,29 @@ drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma,
     return(list(a = a, conv = conv))
 }
 
-drt <- function(mu0, mu1, sigma0, sigma1, sigma,
-                iter = 1, lambda = 0, method = "Penalization") {
+drt <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
+                iter = 1, lambda = 0, method = "Penalization",
+                optim = "BFGS") {
     if (iter > 1 & lambda == 0) {
         p <- length(mu0)
         a <- matrix(0, p, iter)
         conv <- rep(0, iter)
-        drt <- drt_1iter(mu0, mu1, sigma0, sigma1, sigma, lambda, method)
+        drt <- drt_1iter(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
+                         lambda, method, optim)
         a[, 1] <- drt$a
         conv[1] <- drt$conv
         for (j in 2:iter) {
             Q <- qr.Q(qr(a[, 1:(j - 1)]), complete = TRUE)[, j:p]
             drt <- drt_1iter(mu0 = t(Q) %*% mu0, mu1 = t(Q) %*% mu1,
                              sigma0 = t(Q) %*% sigma0 %*% Q,
-                             sigma1 = t(Q) %*% sigma1 %*% Q, lambda, method)
+                             sigma1 = t(Q) %*% sigma1 %*% Q,
+                             p0, p1, lambda, method, optim)
             d <- Q %*% drt$a
             a[, j] <- d / sqrt(sum(d^2))
             conv[j] <- drt$conv
         }
         return(list(a = a, conv = conv))
-    } else return(drt_1iter(mu0, mu1, sigma0, sigma1, sigma, lambda, method))
+    } else
+        return(drt_1iter(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
+                         lambda, method, optim))
 }
--- R/qdap.R
+++ R/qdap.R
@@ -12,23 +12,29 @@
 ##' @author Ruiyang Wu
 ##' @export
 
-qdap <- function(x, y, xnew, lambda = 0, iter = 1, method = "Penalization") {
+qdap <- function(x, y, xnew, lambda = 0, iter = 1,
+                 method = "Penalization", optim = "BFGS") {
     x <- data.matrix(x)
     xnew <- data.matrix(xnew)
     x0 <- x[which(y == 0), ]
     x1 <- x[which(y == 1), ]
+    n0 <- nrow(x0)
+    n1 <- nrow(x1)
+    p0 <- n0 / (n0 + n1)
+    p1 <- n1 / (n0 + n1)
     p <- ncol(x)
     mu0 <- colMeans(x0)
     mu1 <- colMeans(x1)
     sigma0 <- cov(x0)
     sigma1 <- cov(x1)
-    sigma <- ((nrow(x0) - 1) * sigma0 + (nrow(x1) - 1) * sigma1) /
-        (nrow(x0) + nrow(x1) - 1)
-    drt <- drt(mu0, mu1, sigma0, sigma1, sigma, iter, lambda, method)
     if (!is_invertible(sigma0))
         sigma0 <- sigma0 + diag(10e-7, p)
     if (!is_invertible(sigma1))
         sigma1 <- sigma1 + diag(10e-7, p)
+    sigma <- ((n0 - 1) * sigma0 + (n1 - 1) * sigma1) /
+        (n0 + n1 - 2)
+    drt <- drt(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
+               iter, lambda, method, optim)
     a <- drt$a
     ## print(a)
     conv <- drt$conv
