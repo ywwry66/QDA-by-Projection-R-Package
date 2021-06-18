9edfbb6c907bcdafe235772c14e1e991e5aa90d5
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-06-17 20:12:15 -0700

    Several changes

--- R/qdap.R
+++ R/qdap.R
@@ -1,6 +1,6 @@
 drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
                       lambda = 0, method = "Penalization",
-                      par = NULL, optim = "codesc") {
+                      par = NULL, max_iter = 1000, optim = "codesc") {
     ## initialization
     if (method != "Penalization" & method != "Thresholding")
         stop("Wrong method")
@@ -16,20 +16,25 @@ drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
             mis_rate(par, mu0, mu1, sigma0, sigma1, p0, p1))
             par <- par1
     }
+    target <- function(a, mu0, mu1, sigma0, sigma1, p0, p1, lambda)
+        mis_rate(a, mu0, mu1, sigma0, sigma1, p0, p1) +
+            lambda * sum(abs(a)) / sqrt(sum(a ^ 2))
     par <- par / sqrt(sum(par^2))
+    print(max_iter)
     ## optimization using specified method
     if (optim == "BFGS")
-        op <- optim(par = par, fn = mis_rate, method = "BFGS",
+        op <- optim(par = par, fn = target, method = "BFGS",
                     mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
-                    p0 = p0, p1 = p1, control = list(maxit = 1000))
+                    p0 = p0, p1 = p1, lambda = lambda,
+                    control = list(maxit = max_iter))
     else if (optim == "codesc")
-        op <- codesc(par = par, fun = mis_rate, max_iter = 1000,
+        op <- codesc(par = par, fun = target, max_iter = max_iter,
                      mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
-                     p0 = p0, p1 = p1, step_size = 0.1)
+                     p0 = p0, p1 = p1, lambda = lambda)
     else if (optim == "frank_wolfe")
-        op <- frank_wolfe(par = par, fun = mis_rate, max_iter = 500,
+        op <- frank_wolfe(par = par, fun = target, max_iter = max_iter,
                           mu0 = mu0, mu1 = mu1, sigma0 = sigma0, sigma1 = sigma1,
-                          p0 = p0, p1 = p1)
+                          p0 = p0, p1 = p1, lambda = lambda)
     else
         stop("Wrong optimization method")
     ## print(op)
@@ -47,13 +52,14 @@ drt_1iter <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
 
 drt <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
                 iter = 1, lambda = 0, method = "Penalization",
-                optim = "codesc") {
+                par = NULL, max_iter = 1000, optim = "codesc") {
     if (iter > 1 & lambda == 0) {
         p <- length(mu0)
         a <- matrix(0, p, iter)
         conv <- rep(0, iter)
         drt <- drt_1iter(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
-                         lambda, method, optim)
+                         lambda = lambda, method = method,
+                         par = par, max_iter = max_iter, optim = optim)
         a[, 1] <- drt$a
         conv[1] <- drt$conv
         for (j in 2:iter) {
@@ -61,7 +67,8 @@ drt <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
             drt <- drt_1iter(mu0 = t(Q) %*% mu0, mu1 = t(Q) %*% mu1,
                              sigma0 = t(Q) %*% sigma0 %*% Q,
                              sigma1 = t(Q) %*% sigma1 %*% Q,
-                             p0, p1, lambda, method, optim)
+                             p0, p1, lambda = lambda, method = method,
+                             par = par, max_iter = max_iter, optim = optim)
             d <- Q %*% drt$a
             a[, j] <- d / sqrt(sum(d^2))
             conv[j] <- drt$conv
@@ -69,7 +76,8 @@ drt <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
         return(list(a = a, conv = conv))
     } else
         return(drt_1iter(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
-                         lambda, method, optim))
+                         lambda = lambda, method = method,
+                         par = par, max_iter = max_iter, optim = optim))
 }
 
 ##' This function executes the QDA by Projection method
@@ -87,7 +95,8 @@ drt <- function(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
 ##' @export
 
 qdap <- function(x, y, xnew = NULL, lambda = 0, iter = 1,
-                 method = "Penalization", optim = "codesc") {
+                 method = "Penalization", par = NULL,
+                 max_iter = 1000, optim = "codesc") {
     x <- data.matrix(x)
     x0 <- x[which(y == 0), ]
     x1 <- x[which(y == 1), ]
@@ -107,9 +116,11 @@ qdap <- function(x, y, xnew = NULL, lambda = 0, iter = 1,
     sigma <- ((n0 - 1) * sigma0 + (n1 - 1) * sigma1) /
         (n0 + n1 - 2)
     drt <- drt(mu0, mu1, sigma0, sigma1, sigma, p0, p1,
-               iter, lambda, method, optim)
+               iter, lambda, method, par, max_iter, optim)
     a <- drt$a
+    print(a)
     conv <- drt$conv
+    print(conv)
     ## 1D qda
     x <- as.vector(x %*% a)
     x0 <- x[which(y == 0)]
@@ -135,8 +146,20 @@ qdap <- function(x, y, xnew = NULL, lambda = 0, iter = 1,
 ##' @export
 
 qdap_cv <- function(x, y, xnew = NULL, lambda = c(1, 2, 4, 8, 16),
-                    method = "Penalization", optim = "codesc",
+                    method = "Penalization", max_iter = 1000, optim = "codesc",
                     folds = 5, seed = 2020) {
+    if (!is.null(seed)) {
+        ## reinstate system seed after simulation
+        sys_seed <- .GlobalEnv$.Random.seed
+        on.exit({
+            if (!is.null(sys_seed)) {
+                .GlobalEnv$.Random.seed <- sys_seed
+            } else {
+                rm(".Random.seed", envir = .GlobalEnv)
+            }
+        })
+        set.seed(seed)
+    }
     x <- data.matrix(x)
     x0 <- x[which(y == 0), ]
     x1 <- x[which(y == 1), ]
@@ -147,48 +170,38 @@ qdap_cv <- function(x, y, xnew = NULL, lambda = c(1, 2, 4, 8, 16),
     folds0 <- cut(seq_len(n0), breaks = folds, labels = FALSE)
     folds1 <- cut(seq_len(n1), breaks = folds, labels = FALSE)
     pred_err <- rep(0, length(lambda))
-    for (j in seq_len(lambda)) {
+    par <- rep(list(NULL), folds)
+    for (j in seq_along(lambda)) {
         for (i in seq_len(folds)) {
             test_ind0 <- which(folds0 == i)
             test_ind1 <- which(folds1 == i)
             test_n0 <- length(test_ind0)
             test_n1 <- length(test_ind1)
-            ypred <-
-                qdap(x = rbind(x0[-ind0, ], x1[-ind1, ]),
+            fit <-
+                qdap(x = rbind(x0[-test_ind0, ], x1[-test_ind1, ]),
                      y = c(rep(0, n0 - test_n0),
                            rep(1, n1 - test_n1)),
-                     xnew = rbind(x0[ind0, ], x1[ind1, ]),
+                     xnew = rbind(x0[test_ind0, ], x1[test_ind1, ]),
                      lambda = lambda[j],
                      method = method,
-                     optim = optim)$class
+                     par = par[[i]],
+                     max_iter = max_iter,
+                     optim = optim)
+            par[[i]] <- fit$drt
+            ## print(fit$drt)
+            ypred <- fit$class
             pred_err[j] <- pred_err[j] +
-                sum(ypred != c(rep(0, test_n0), rep(0, test_n1)))
+                sum(ypred != c(rep(0, test_n0), rep(1, test_n1)))
         }
     }
+    print(pred_err)
     lambda_best <- lambda[which.min(pred_err)]
-    return(qdap(x, y, xnew,
+    out <- qdap(x, y, xnew,
                 lambda = lambda_best,
                 method = method,
-                optim = optim))
+                max_iter = max_iter,
+                optim = optim)
+    print(lambda_best)
+    ## print(out$drt)
+    return(c(out, list(lambda = lambda_best)))
 }
-
-## mylda <- function(x, y, xnew) {
-##     x <- data.matrix(x)
-##     xnew <- data.matrix(xnew)
-##     x0 <- as.matrix(x[which(y == 0), ])
-##     x1 <- as.matrix(x[which(y == 1), ])
-##     p <- ncol(x)
-##     n0 <- nrow(x0)
-##     n1 <- nrow(x1)
-##     mu0 <- colMeans(x0)
-##     mu1 <- colMeans(x1)
-##     sigma0 <- cov(x0)
-##     sigma1 <- cov(x1)
-##     sigma <- ((n0 - 1) * sigma0 + (n1 - 1) * sigma1) /
-##         (n0 + n1 - 2)
-##     a0 <- solve(sigma + diag(0.0000001, p)) %*% (mu1 - mu0)
-##     if (p == 1) a0 <- solve(sigma) %*% (mu1 - mu0)
-##     pred <- function(xnew) xnew %*% a0 > (mu0 + mu1) %*% a0 / 2 - log(n1 / n0)
-##     ynew <- apply(xnew, MARGIN = 1, FUN = pred)
-##     return(list(class = ynew))
-## }
