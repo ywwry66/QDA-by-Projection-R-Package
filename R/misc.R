df916a641550c812c2c2ba966e2320e6b8cd1a42
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-11-02 15:15:50 -0700

    Add a function to calculate the precision matrix

--- R/misc.R
+++ R/misc.R
@@ -11,3 +11,13 @@ maxquadratio <- function(sigma0, sigma1) {
 ## Test whether a matrix is invertible.
 ## If so, return TRUE; otherwise, return FALSE.
 is_invertible <- function(m) class(try(solve(m), silent = T))[1] == "matrix"
+
+## Calculate the precision matrix (generalized inverse of covariance)
+## from centered data.
+prec <- function(x, df = nrow(x) - 1, tol = 1e-4) {
+    x_svd <- svd(x, nu = 0)
+    rank <- sum(x_svd$d > tol)
+    prec <- x_svd$v[, 1:rank] %*% diag(1 / x_svd$d[1:rank]) %*%
+        t(x_svd$v[, 1:rank] %*% diag(1 / x_svd$d[1:rank])) * df
+    return(prec)
+}
