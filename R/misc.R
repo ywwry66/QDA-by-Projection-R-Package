e5b6e9acba7b92101b66a73544a20d1f4a1bc17f
Ruiyang Wu <58066925+ywwry66@users.noreply.github.com>
2021-11-02 14:49:24 -0700

    Update documentation

--- R/misc.R
+++ R/misc.R
@@ -1,11 +1,6 @@
-##' Maximizing the ratio of two full rank quadratic forms
-##'
-##' Find the direction `a' that maximizes (t(a) %*% sigma0 %*% a)/(t(a) %*% sigma1 %*% a)
-##' @title Maximizing the ratio of two full rank quadratic forms
-##' @param sigma0 The matrix which determines the top full rank quadratic form
-##' @param sigma1 The matrix which determines the bottom full rank quadratic form
-##' @return
-##' @author Ruiyang Wu
+## Maximize the ratio of two full rank quadratic forms
+## r(a) := (t(a) %*% sigma0 %*% a)/(t(a) %*% sigma1 %*% a).
+## Return a list with the optimal direction and the maximum ratio.
 maxquadratio <- function(sigma0, sigma1) {
     ei <- eigen(solve(sigma1) %*% sigma0, symmetric = TRUE)
     arg <- as.vector(ei$vectors[, 1])
@@ -13,4 +8,6 @@ maxquadratio <- function(sigma0, sigma1) {
     return(list(arg = arg, value = value))
 }
 
+## Test whether a matrix is invertible.
+## If so, return TRUE; otherwise, return FALSE.
 is_invertible <- function(m) class(try(solve(m), silent = T))[1] == "matrix"
